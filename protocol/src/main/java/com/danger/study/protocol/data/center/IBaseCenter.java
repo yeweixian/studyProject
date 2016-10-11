462688c9f7171b9f6ce7cb7177b9d858e8bba44a
yeweixian <yeweixian@aipai.com>
2016-10-11 16:52:35 +0800

    项目代码提交

---
+++ protocol/src/main/java/com/danger/study/protocol/data/center/IBaseCenter.java
@@ -0,0 +1,7 @@
+package com.danger.study.protocol.data.center;
+
+/**
+ * Created by PC-361 on 2016/10/11.
+ */
+public interface IBaseCenter {
+}
---
+++ service/dataServer/src/main/java/com/danger/study/center/BaseCenter.java
@@ -0,0 +1,9 @@
+package com.danger.study.center;
+
+import com.danger.study.protocol.data.center.IBaseCenter;
+
+/**
+ * Created by PC-361 on 2016/10/11.
+ */
+public class BaseCenter implements IBaseCenter {
+}
