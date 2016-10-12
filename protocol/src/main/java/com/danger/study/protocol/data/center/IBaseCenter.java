0994ac11a9f2cd1b56ab9d894b5c67b7fba00b60
yeweixian <yeweixian@aipai.com>
2016-10-12 14:05:21 +0800

    功能代码提交

--- protocol/src/main/java/com/danger/study/protocol/data/center/IBaseCenter.java
+++ protocol/src/main/java/com/danger/study/protocol/data/center/IBaseCenter.java
@@ -4,4 +4,6 @@ package com.danger.study.protocol.data.center;
  * Created by PC-361 on 2016/10/11.
  */
 public interface IBaseCenter {
+
+    String getBaseMsg();
 }
--- service/dataServer/src/main/java/com/danger/study/dataserver/center/BaseCenter.java
+++ service/dataServer/src/main/java/com/danger/study/dataserver/center/BaseCenter.java
@@ -6,4 +6,9 @@ import com.danger.study.protocol.data.center.IBaseCenter;
  * Created by PC-361 on 2016/10/11.
  */
 public class BaseCenter implements IBaseCenter {
+
+    @Override
+    public String getBaseMsg() {
+        return "BaseMsg";
+    }
 }
