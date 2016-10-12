15bd731cb169d6bb40d934fbd4297c3ea3bfcf5b
yeweixian <yeweixian@aipai.com>
2016-10-12 11:36:11 +0800

    功能代码提交

--- service/dataServer/src/main/java/com/danger/study/dataserver/Application.java
+++ service/dataServer/src/main/java/com/danger/study/dataserver/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
 
 /**
  * Created by PC-361 on 2016/10/9.
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @EnableAutoConfiguration
 @ComponentScan
+@ImportResource({"classpath:applicationContext.xml"})
 public class Application {
 
     public static void main(String[] args) {
