37a8e63603ad5bf65d964e1c53b05f02437af3ce
yeweixian <yeweixian@aipai.com>
2016-11-18 16:01:27 +0800

    添加类和查询

---
+++ protocol/src/main/java/com/danger/study/protocol/data/entity/User.java
@@ -0,0 +1,36 @@
+package com.danger.study.protocol.data.entity;
+
+import java.io.Serializable;
+
+/**
+ * Created by PC-361 on 2016/11/18.
+ */
+public class User implements Serializable {
+    private long id;
+    private String name;
+    private String password;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
