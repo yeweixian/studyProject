87a2c981b133bacaa3af448c47c55f83a1810454
yeweixian <yeweixian@aipai.com>
2016-11-18 16:20:29 +0800

    提交测试类

--- service/dataServer/src/test/java/com/danger/study/dataserver/center/BaseCenterTest.java
+++ service/dataServer/src/test/java/com/danger/study/dataserver/center/BaseCenterTest.java
@@ -4,17 +4,15 @@ import com.danger.study.dataserver.Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 
 /**
  * Created by PC-361 on 2016/11/18.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = Application.class)
-@WebAppConfiguration
+@ContextConfiguration(classes = Application.class)
 public class BaseCenterTest {
 
     @Autowired
