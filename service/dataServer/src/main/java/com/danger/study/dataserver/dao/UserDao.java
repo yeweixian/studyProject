f2df43aac5b89fc6aff687f2d25d491f92aafa0a
yeweixian <yeweixian@aipai.com>
2016-11-21 16:54:14 +0800

    代码提交

--- service/dataServer/src/main/java/com/danger/study/dataserver/dao/UserDao.java
+++ service/dataServer/src/main/java/com/danger/study/dataserver/dao/UserDao.java
@@ -1,10 +1,27 @@
 package com.danger.study.dataserver.dao;
 
+import com.danger.study.dataserver.helper.MybatisHelper;
+import com.danger.study.dataserver.mapper.UserMapper;
+import com.danger.study.protocol.data.entity.User;
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * Created by PC-361 on 2016/11/18.
  */
 @Component
 public class UserDao {
+
+    @Autowired
+    private MybatisHelper mybatisHelper;
+
+    public List<User> findAllUser() {
+        try (SqlSession session = mybatisHelper.openSession()) {
+            UserMapper userMapper = session.getMapper(UserMapper.class);
+            return userMapper.findAllUser();
+        }
+    }
 }
