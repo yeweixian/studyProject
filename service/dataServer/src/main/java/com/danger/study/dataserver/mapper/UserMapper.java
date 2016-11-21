991d8f73b429097090cf14551e448cf3b72e8f1b
yeweixian <yeweixian@aipai.com>
2016-11-21 16:29:48 +0800

    代码提交

--- service/dataServer/src/main/java/com/danger/study/dataserver/mapper/UserMapper.java
+++ service/dataServer/src/main/java/com/danger/study/dataserver/mapper/UserMapper.java
@@ -1,17 +1,12 @@
 package com.danger.study.dataserver.mapper;
 
 import com.danger.study.protocol.data.entity.User;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 /**
  * Created by PC-361 on 2016/11/18.
  */
-@Mapper
 public interface UserMapper {
-
-    @Select("select * from user")
     List<User> findAllUser();
 }
