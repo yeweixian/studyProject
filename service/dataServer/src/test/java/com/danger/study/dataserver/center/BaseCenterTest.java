49af6cdb3f4269045db8664accb90c3d5c70b226
yeweixian <yeweixian@aipai.com>
2016-11-21 16:58:25 +0800

    代码提交

--- service/dataServer/src/test/java/com/danger/study/dataserver/center/BaseCenterTest.java
+++ service/dataServer/src/test/java/com/danger/study/dataserver/center/BaseCenterTest.java
@@ -1,12 +1,17 @@
 package com.danger.study.dataserver.center;
 
 import com.danger.study.dataserver.Application;
+import com.danger.study.dataserver.dao.UserDao;
+import com.danger.study.protocol.data.entity.User;
+import com.danger.study.tools.common.JsonUtils;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
 
 /**
  * Created by PC-361 on 2016/11/18.
@@ -23,4 +28,18 @@ public class BaseCenterTest {
         BaseCenter baseCenter = context.getBean(BaseCenter.class);
         System.out.println("--- test msg: " + baseCenter.getBaseMsg());
     }
+
+    @Test
+    public void testFindAllUser() {
+        UserDao userDao = context.getBean(UserDao.class);
+        List<User> userList = userDao.findAllUser();
+        System.out.println("----------------------------");
+        for (User user : userList) {
+            System.out.println(JsonUtils.getJsonString(user));
+        }
+        System.out.println("----------------------------");
+    }
+
+    @Test
+    public void test() {}
 }
