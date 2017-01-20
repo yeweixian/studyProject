6e4bf127a1e80cdaed7d2577fdb8a8a455723b69
yeweixian <yeweixian@aipai.com>
2017-01-20 19:29:09 +0800

    环境添加 redis

--- service/dataServer/src/test/java/com/danger/study/dataserver/redis/BaseRedisTest.java
+++ service/dataServer/src/test/java/com/danger/study/dataserver/redis/BaseRedisTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import redis.clients.jedis.Jedis;
 
@@ -27,6 +28,11 @@ public class BaseRedisTest {
         Jedis jedis = (Jedis) jedisConnectionFactory.getConnection().getNativeConnection();
         jedis.set("test", "test");
         jedis.close();
+    }
+
+    public void testRedisTemplate() {
+        RedisTemplate<String, String> template = context.getBean(RedisTemplate.class);
+        template.opsForValue().set("RedisTemplate", "RedisTemplate");
     }
 
     @Test
