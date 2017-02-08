11d16141b26ee8f385bcc7b12210e01c865edcb0
yeweixian <yeweixian@aipai.com>
2017-02-08 11:54:11 +0800

    分布式锁模块代码提交

--- lock/src/main/java/com/danger/study/lock/tools/LockHelper.java
+++ lock/src/main/java/com/danger/study/lock/tools/LockHelper.java
@@ -9,14 +9,20 @@ import org.springframework.stereotype.Component;
 @Component
 public class LockHelper {
 
+    public static final int DEFAULT_WAITTIME = 4000;
+    public static final int DEFAULT_TIMEOUT = 2000;
+
     @Autowired
     private LockRedisHelper lockRedisHelper;
 
     public boolean lock(String lockKey, long waitTime, long timeout) throws InterruptedException {
+        long beginTime = System.currentTimeMillis();
         //使用 setnx 判断是否 获得锁
         Long result = lockRedisHelper.process(jedis -> jedis.setnx(lockKey, String.valueOf(System.currentTimeMillis() + timeout)));
         if (result == 1) return true;
         while (true) {
+            //超时返回 false 表示 没有获取 锁
+            if (beginTime + waitTime < System.currentTimeMillis()) return false;
             //使用 get 获取 上次锁的 销毁时间
             long oldExpireTime = Long.valueOf(lockRedisHelper.process(jedis -> jedis.get(lockKey)));
             if (oldExpireTime < System.currentTimeMillis()) {
