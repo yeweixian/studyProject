714480172198a4f3841442950c3f26f1ab4dd4d9
yeweixian <yeweixian@aipai.com>
2017-04-05 11:55:34 +0800

    添加aop环境代码

--- service/dataServer/src/main/java/com/danger/study/dataserver/aspect/TestAspect.java
+++ service/dataServer/src/main/java/com/danger/study/dataserver/aspect/TestAspect.java
@@ -1,7 +1,28 @@
 package com.danger.study.dataserver.aspect;
 
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
 /**
  * Created by PC-361 on 2017/4/5.
  */
+@Aspect
+@Component
 public class TestAspect {
+
+    @Pointcut("execution(public * com.danger.study.dataserver..*.*(..))")
+    public void test() {}
+
+    @Before("test()")
+    public void doBefore() {
+        System.out.println(" ------ AOP doBefore function!");
+    }
+
+    @AfterReturning(returning = "ret", pointcut = "test()")
+    public void doAfterReturning(Object ret) {
+        System.out.println(" ------ AOP doAfterReturning: " + ret);
+    }
 }
